80e9c2a0017811a3d0201f2c3423f8ca2ea82104
Christopher Jenkins <chris.jenkins@lvt.com>
2023-10-06 10:48:40 -0600

    use react-navigation

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+const linking = {
+  config: {
+    screens: {
+      Home: "",
+      About: "About",
+    },
+  },
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer linking={linking}>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen}/>
+        <Stack.Screen name="About" component={AboutScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
+const styles = {
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+};
+
+const HomeScreen = ({ navigation }) => <View style={styles.container}>
+  <Text>HomeScreen</Text>
+  <Button title="Go to About" onPress={() => navigation.navigate("About")}/>
+</View>;
+
+const AboutScreen = () => <View style={styles.container}>
+  <Text>AboutScreen</Text>
+</View>;
