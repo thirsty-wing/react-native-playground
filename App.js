89780cecc9a38e643ab121b448cb041d9c9510ff
Christopher Jenkins <chris.jenkins@lvt.com>
2023-10-10 17:02:07 -0600

    can sort

--- App.js
+++ App.js
@@ -1,7 +1,16 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Button, Text, View } from 'react-native';
+import { Button, Pressable, ScrollView, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
+import data from './data';
 
 const Stack = createNativeStackNavigator();
 
@@ -27,10 +36,7 @@ export default function App() {
 
 const styles = {
   container: {
-    flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 };
 
@@ -39,6 +45,117 @@ const HomeScreen = ({ navigation }) => <View style={styles.container}>
   <Button title="Go to About" onPress={() => navigation.navigate("About")}/>
 </View>;
 
-const AboutScreen = () => <View style={styles.container}>
-  <Text>AboutScreen</Text>
-</View>;
+const columnHelper = createColumnHelper();
+
+const columns = [
+  columnHelper.accessor('firstName', {
+    cell: info => <Text>{info.getValue()}</Text>,
+    footer: info => info.column.id,
+  }),
+  columnHelper.accessor(row => row.lastName, {
+    id: 'lastName',
+    cell: info => <Text>{info.getValue()}</Text>,
+    header: () => <Text>Last Name</Text>,
+    footer: info => info.column.id,
+  }),
+  columnHelper.accessor('age', {
+    header: () => <Text>Age</Text>,
+    cell: info => <Text>{info.renderValue()}</Text>,
+    footer: info => info.column.id,
+  }),
+  columnHelper.accessor('visits', {
+    header: () => <Text>Visits</Text>,
+    footer: info => info.column.id,
+  }),
+  columnHelper.accessor('status', {
+    header: <Text>Status</Text>,
+    footer: info => info.column.id,
+  }),
+  columnHelper.accessor('progress', {
+    header: <Text>Profile Progress</Text>,
+    footer: info => info.column.id,
+  }),
+]
+
+const AboutScreen = () => {
+
+  const [sorting, setSorting] = React.useState([])
+
+  const table = useReactTable({
+    columns,
+    data,
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    onSortingChange: setSorting,
+    state: {
+      sorting,
+    },
+  });
+
+  return (
+    <View>
+      <View style={{ alignItems: 'center', position: 'sticky' }}>
+        <Text>AboutScreen</Text>
+      </View>
+
+
+      <ScrollView horizontal={true} contentContainerStyle={{ flexDirection: 'column' }}>
+        <View style={{ backgroundColor: '#DDD' }}>
+          { table.getHeaderGroups().map(headerGroup => (
+              <View key={headerGroup.id} style={{ flexDirection: 'row', gap: 10 }}>
+                { headerGroup.headers.map(header => (
+                  <Pressable
+                    key={header.id}
+                    style={{
+                      height: 25,
+                      justifyContent: 'space-around',
+                      width: 125,
+                    }}
+                    onPress={header.column.getToggleSortingHandler()}
+                  >
+                    <Text>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext(),
+                        )
+                      }
+                      {{
+                        asc: ' 🔼',
+                        desc: ' 🔽',
+                      }[header.column.getIsSorted()] ?? null}
+                    </Text>
+                  </Pressable>
+                ))}
+              </View>
+          ))}
+        </View>
+
+    <ScrollView contentContainerStyle={styles.container}>
+          { table.getRowModel().rows.map(row => (
+            <View key={row.id} style={{ flexDirection: 'row', gap: 10 }}>
+            { row.getVisibleCells().map(cell => (
+              <View
+                key={cell.id}
+                style={{
+                  height: 25,
+                  justifyContent: 'space-around',
+                  width: 125,
+                }}
+              >
+                <Text>
+                  {flexRender(
+                    cell.column.columnDef.cell, cell.getContext(),
+                  )}
+                </Text>
+              </View>
+            ))}
+            </View>
+          ))}
+      </ScrollView>
+
+    </ScrollView>
+    </View>
+  );
+};
